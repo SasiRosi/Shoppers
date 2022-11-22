d6a264e78e3844c679115394cf4a47212a88b0f0
SalvatoreDelPiano <s.delpiano306@gmail.com>
2022-11-22 15:41:08 +0000

    news

---
+++ shop/index.js
@@ -0,0 +1,14 @@
+var express= require("express");
+const path= require("path");
+
+var app=express();
+
+app.get("/", function(req,res){
+
+res.sendFile(path.join(__dirname,"/index.html"));
+
+});
+
+app.listen(3000,function(){
+    console.log("siamo entrati");
+});

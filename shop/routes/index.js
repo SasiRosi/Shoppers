89290078d8a353a79ebf46b0d205e6ba284d472e
S.DelPiano <s.delpiano306@gmail.com>
2022-11-24 18:01:34 +0100

    login

--- shop/routes/index.js
+++ shop/routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'JustDoShop' });
+});
+
+router.get('/page-1', function(req, res, next) {
+  res.render('page1', { title: 'JustDoShop' });
+});
+
+router.get('/page-2', function(req, res, next) {
+  res.render('page2', { title: 'JustDoShop' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'JustDoShop' });
+});
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'JustDoShop' });
 });
 
 module.exports = router;

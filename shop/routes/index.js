aac953852005d760970bc891d5252a6813b97385
S.DelPiano <s.delpiano306@gmail.com>
2022-11-25 00:39:10 +0100

    login

--- shop/routes/index.js
+++ shop/routes/index.js
@@ -19,5 +19,10 @@ router.get('/login', function(req, res, next) {
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'JustDoShop' });
 });
+router.get('/account', function(req, res, next) {
+  res.render('account', { title: 'JustDoShop' });
+});
+
+
 
 module.exports = router;
